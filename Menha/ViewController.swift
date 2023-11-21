5cdf17b89bfcb85845bb345fe235488bc5c31bfb
seokki96 <kwons4163@naver.com>
2023-11-22 00:16:39 +0900

    Initial Commit

---
+++ Menha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Menha
+//
+//  Created by SeokkiKwon on 11/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
