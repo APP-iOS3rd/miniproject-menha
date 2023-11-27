3c411b9e52a6e54cc9b97d21593c2bac64df9a19
seokki96 <kwons4163@naver.com>
2023-11-28 04:11:19 +0900

    [feat&fix] 앱실행시 로그인 유지, 화면이동 기능 추가 및 오류 수정 - 앱실행시 로그인 유지가 안되는 문제 처리(SceneDelegate.swift 파일 수정) -  로그인 했을때 로그인 화면으로 돌아가지는 문제 처리(navigationContoroller 비우는 방식으로 해결) - 로그아웃 기능 추가 - 멘토, 멘티 유형에 따라서 다른 화면으로 이동하는 로직 추가

--- MenHa/MenHa/Model/UserService.swift
+++ MenHa/MenHa/Model/UserService.swift
@@ -9,6 +9,11 @@ import Foundation
 import FirebaseAuth
 import FirebaseFirestore
 
+enum AuthError: Error {
+    case noneUserSession
+    case noneUser
+}
+
 class UserService {
     static let shared = UserService()
     var userId: String?
@@ -20,13 +25,48 @@ class UserService {
     }
     
     // 현재 로그인한 유저의 정보 리턴
-    func getCurrentUser(completion: @escaping (User) -> ()) {
-        guard let userId = userId else {return}
-        // User 구조체 형태로 현재 로그인한 유저의 정보를 리턴
-        db.collection("users").document(userId).getDocument { snapshot, _ in
-            guard let user = try? snapshot?.data(as: User.self) else { return }
-            self.currentUser = user
-            completion(user)
+    func getCurrentUser(completion: @escaping (Result<User, Error>) -> ()) {
+        if let userId = userId {
+            // User 구조체 형태로 현재 로그인한 유저의 정보를 리턴
+            db.collection("users").document(userId).getDocument { snapshot, error in
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    guard let user = try? snapshot?.data(as: User.self) else { return }
+                    self.currentUser = user
+                    completion(.success(user))
+                }
+            }
+        } else {
+            completion(.failure(AuthError.noneUserSession))
+        }
+    }
+    
+    // 로그인
+    func login(email: String, password: String, completion: @escaping (Result<User, Error>) -> ()){
+        Auth.auth().signIn(withEmail: email, password: password) {(user, error) in
+            // user email, password가 일치하지 않는경우
+            guard let user = user else {
+                completion(.failure(AuthError.noneUser))
+                return
+            }
+            // 스토어에 별도로 저장된 유저정보
+            self.db.collection("users").document(user.user.uid).getDocument { snapShot, error in
+                guard let userData = try? snapShot?.data(as: User.self)  else {return}
+                self.currentUser = userData
+                completion(.success(self.currentUser!))
+            }
+        }
+    }
+    
+    // 로그아웃
+    func logout(completion: @escaping (Bool) -> ()) {
+        do {
+            try Auth.auth().signOut()
+            self.currentUser = nil
+            completion(true)
+        } catch {
+            completion(false)
         }
     }
 }
--- MenHa/MenHa/ViewController/Main/MainViewController.swift
+++ MenHa/MenHa/ViewController/Main/MainViewController.swift
@@ -8,41 +8,9 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func goLoginScreen(_ sender: Any) {
-        let loginStoryBoard = UIStoryboard(name: Login.name.rawValue, bundle: nil)
-        // Instantiate the desired view controller from the second storyboard
-        if let loginViewController = loginStoryBoard.instantiateViewController(withIdentifier: Login.id.rawValue) as? LoginViewController {
-            
-            // Perform the navigation
-            self.navigationController?.pushViewController(loginViewController, animated: true)
-        }
-    }
-    
-    @IBAction func goMentoScreen(_ sender: Any) {
-        let mentoStorayBoard = UIStoryboard(name: Mento.name.rawValue, bundle: nil)
-        
-        // Instantiate the desired view controller from the second storyboard
-        if let mentoViewController = mentoStorayBoard.instantiateViewController(withIdentifier: Mento.id.rawValue) as? MentoViewController {
-            
-            // Perform the navigation
-            self.navigationController?.pushViewController(mentoViewController, animated: true)
-        }
-    }
-    
-    @IBAction func goMenteeScreen(_ sender: Any) {
-        let menteeStoryboard = UIStoryboard(name: Mentee.name.rawValue, bundle: nil)
-        
-        // Instantiate the desired view controller from the second storyboard
-        if let menteeViewController = menteeStoryboard.instantiateViewController(withIdentifier: Mentee.id.rawValue) as? MenteeViewController {
-            
-            // Perform the navigation
-            self.navigationController?.pushViewController(menteeViewController, animated: true)
-        }
-    }
 }
