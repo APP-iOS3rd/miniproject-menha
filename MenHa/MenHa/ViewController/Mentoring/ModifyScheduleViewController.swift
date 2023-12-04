3dc5e444847eca10111a64914b3b789b82e2d70a
zxl3651 <zxl3651@naver.com>
2023-12-05 00:02:37 +0900

    [feat] ModifyScheduleViewController와 ModifySchedule.storyboard 연결 완료

--- MenHa/MenHa/ViewController/Mentoring/ModifyScheduleViewController.swift
+++ MenHa/MenHa/ViewController/Mentoring/ModifyScheduleViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class ModifyScheduleViewController: UIViewController {
+    
+    @IBOutlet weak var groupname: UITextField!
+    @IBOutlet weak var subject: UITextField!
+    @IBOutlet weak var referencelinks: UITextView!
+    @IBOutlet weak var isgroup: UISegmentedControl!// selectedSegmentIndex( 개인 == 0, 그룹 == 1 )
+    @IBOutlet weak var progressdate: UIDatePicker!
 
+    // 건네받은 Schedule 모델을 토대로 각 textfield, textview, datepicker 재구성하기
+    var modifySchedule : receiveSchedule?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
