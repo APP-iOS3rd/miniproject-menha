c4e9b20ca6a2b54eb730b83802dd8c57b39d1362
hyunseo <hyunseo3347@gmail.com>
2023-12-12 18:56:13 +0900

    공유기능 추가

--- MenHa/MenHa/ViewController/Mentoring/CreateInvieteCodeViewController.swift
+++ MenHa/MenHa/ViewController/Mentoring/CreateInvieteCodeViewController.swift
@@ -9,13 +9,28 @@ import UIKit
 
 class CreateInvieteCodeViewController: UIViewController {
 
+    @IBOutlet var inviteCodeTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func shareInviteCode(_ sender: Any) {
+        let text = "초대코드입니다. \(inviteCodeTextField)"
+               
+               // set up activity view controller
+               let textToShare = [ text ]
+               let activityViewController = UIActivityViewController(activityItems: textToShare, applicationActivities: nil)
+               activityViewController.popoverPresentationController?.sourceView = self.view // so that iPads won't crash
+               
+               // exclude some activity types from the list (optional)
+               activityViewController.excludedActivityTypes = [ UIActivity.ActivityType.airDrop, UIActivity.ActivityType.postToFacebook ]
+               
+               // present the view controller
+               self.present(activityViewController, animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
